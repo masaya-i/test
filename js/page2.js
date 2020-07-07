38566c85189016af1813a25971ccd35fb97b1378
masaya-i <kiyozonbi@gmail.com>
2020-07-07 22:15:55 +0900

    menuバー追加

--- js/page2.js
+++ js/page2.js
@@ -1,9 +1,8 @@
-// ナビバーの開閉を設定
-for (const burder of document.getElementsByClassName('navbar-burger')) {
-    const menuId = burder.dataset.target;
-    const menu = document.getElementById(menuId);
-    burder.addEventListener('click', e => {
-        burder.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+$(document).ready(function(){
+    var burger = $('.burger');
+    var menu = $('#' + burger.data('target'));
+    burger.on('click', function() {
+      burger.toggleClass('is-active');
+      menu.toggleClass('is-active');
     });
-}
+  });
